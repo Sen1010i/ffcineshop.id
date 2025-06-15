316ad33f69490171595455beed77e431e59c9669
Sen1010i <lantai2tv2@gmail.com>
2025-06-15 10:02:41 +0700

    je

--- script.js
+++ script.js
@@ -158,11 +158,11 @@ if (document.getElementById('formPemesanan')) {
 *Waktu Booking* : ${escapeInput(formData.waktu)}
 *Catatan* : ${formData.catatan ? escapeInput(formData.catatan) : "-"}
 
-Terima kasih!! Sudah melakukan order! Ditunggu ya, admin akan segera membalas pesan anda!
+Terima kasih!! 💕 Sudah melakukan order! Ditunggu ya, admin akan segera membalas pesan anda!
 
 *NOTE:*
 Jika admin belum membalas pesan anda, mungkin sedang melayani customer, atau sedang bobok manis. 
-Tapi tenang, pesanmu tetap kami utamakan!
+Tapi tenang, pesanmu tetap kami utamakan!🥇🤩
 
 Thank you!!!`;
 
