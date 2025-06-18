7763262e44b7c5295d14ce0e40809e87f497ae75
Sen1010i <lantai2tv2@gmail.com>
2025-06-18 23:12:53 +0700

    je

--- script.js
+++ script.js
@@ -161,14 +161,15 @@ if (document.getElementById('formPemesanan')) {
 Terima kasih!! ✨🥰 Sudah melakukan order! Ditunggu ya, admin akan segera membalas pesan anda!
 
 Silakan melakukan pembayaran terlebih dahulu melalui BCA dan Shoppe Pay di bawah ya.😊
+
 Pembayaran tersebut hanya untuk *DEPOSIT* seharga Rp. 50.000!
 
 _Jika sudah melakukan pembayaran silahkan ditunggu konfirmasi ya_
 
--------------------------------
+----------------------------------------------------------------
 *BCA* : FRANJESSCA (0520473165)
 *Shoppe Pay* : FRANJESSCAFC (081268556553)
--------------------------------
+----------------------------------------------------------------
 
 *NOTE:*
 Jika admin belum membalas pesan anda, mungkin sedang melayani customer, atau sedang dalam kendala. 
